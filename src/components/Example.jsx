75bef92c321278a8ee63a8cc51690dc6c0283b1d
eugeniucc <160481703+eugeniucc@users.noreply.github.com>
2024-09-04 22:38:58 +0300

    test

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -13,7 +13,7 @@ export default function Example() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl sm:text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Simple no-tricks pricing
+            Simple no-tricks pricing ssss
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et
